993f0c550e023abe5048552985ee249e8948cbbc
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-02-04 04:29:13 +0000

    serial implementation of gauss

---
+++ cowichan_mpi/gauss/gauss.cpp
@@ -0,0 +1,95 @@
+/**
+ * Generic gauss implementation
+ *
+ * \file randmat.cpp
+ * \author Andrew Borzenko
+ * \date 02-03-09
+ */
+
+#include "../include/main.h"
+#ifdef IS_PARALLEL
+  #include "parallel.h"
+#else
+  #include "serial.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+#ifdef IS_PARALLEL
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+
+  printf ("I am process %d\n", world.rank ());
+
+  real2D	matrix;			/* to multiply by */
+  real1D	vector = {197,129,175,209,125,228,152,176,118,262};			/* to be multiplied */
+  real1D	answer;			/* result of multiply */
+  int		n;			/* matrix size */
+  int limit;
+  int i, j;
+
+  //srand ((unsigned int) time (NULL));
+  srand (222);
+
+  n = MAXEXT;
+  limit = 10;
+
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      matrix[i][j] = rand () % limit;
+    }
+  }
+
+  printf ("\n");
+
+  printf ("Matrix\n");
+  print_matrix (matrix, n, n);
+
+  printf ("Target\n");
+  print_vector (vector, n);
+
+  gauss_mpi (world, matrix, vector, answer, n);
+
+  printf ("Answer\n");
+  print_vector (answer, n);
+#else
+  real2D	matrix;			/* to multiply by */
+  real1D	vector = {197,129,175,209,125,228,152,176,118,262};			/* to be multiplied */
+  real1D	answer;			/* result of multiply */
+  int		n;			/* matrix size */
+  int limit;
+  int i, j;
+
+  //srand ((unsigned int) time (NULL));
+  srand (222);
+
+  n = MAXEXT;
+  limit = 10;
+
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      matrix[i][j] = rand () % limit;
+    }
+  }
+
+  printf ("\n");
+
+  printf ("Matrix\n");
+  print_matrix (matrix, n, n);
+
+  printf ("Target\n");
+  print_vector (vector, n);
+
+  gauss (matrix, vector, answer, n);
+
+  printf ("Answer\n");
+  print_vector (answer, n);
+
+#endif
+
+	return 0;
+}
---
+++ cowichan_mpi/gauss/parallel.h
@@ -0,0 +1,23 @@
+/**
+ * Parallel implementation of generic gauss
+ *
+ * \file parallel.h
+ * \author Andrew Borzenko
+ * \date 02-03-09
+ */
+
+#pragma once
+#ifndef PARALLEL_H
+#define PARALLEL_H
+
+/*--------------------------------------------------------------*/
+/* public functions						*/
+/*--------------------------------------------------------------*/
+
+void gauss_mpi (mpi::communicator world,
+                real2D	matrix,			/* to solve */
+                real1D	vector,			/* target vector */
+                real1D	answer,			/* solution found */
+                int		n);			/* size */
+
+#endif /* PARALLEL_H */
