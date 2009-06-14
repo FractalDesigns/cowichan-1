6369d80c5919bacbb908e29eba0739a69ff41788
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-06-14 17:53:57 +0000

    openmp randmat

--- cowichan_openmp/cowichan_openmp.cpp
+++ cowichan_openmp/cowichan_openmp.cpp
@@ -2,26 +2,15 @@
 
 int main(int argc, char* argv[])
 {
-  int th_id, nthreads;
-#pragma omp parallel private(th_id)
-  {
-    th_id = omp_get_thread_num();
-    std::cout << "Hello World from thread" << th_id << "\n";
-#pragma omp barrier
-    if ( th_id == 0 ) {
-      nthreads = omp_get_num_threads();
-      std::cout << "There are " << nthreads << " threads\n";
-    }
-  }
-
   Cowichan* openmp = new CowichanOpenMP ();
+
+  //omp_set_num_threads(1);
 
   openmp->main(argc, argv, false, true);
 
   return 0;
 }
 
-void CowichanOpenMP::randmat(IntMatrix /* matrix */) {}
 void CowichanOpenMP::half(IntMatrix /* matrixIn */, IntMatrix /* matrixOut */) {}
 void CowichanOpenMP::invperc(IntMatrix /* matrix */, BoolMatrix /* mask */) {}
 void CowichanOpenMP::thresh(IntMatrix /* matrix */, BoolMatrix /* mask */) {}
---
+++ cowichan_openmp/randmat.cpp
@@ -0,0 +1,39 @@
+#include "cowichan_openmp.hpp"
+
+void CowichanOpenMP::randmat (IntMatrix matrix)
+{
+  IntVector state = NULL;
+
+  // initialize first column values
+  try {
+    state = NEW_VECTOR_SZ(uint, nr);
+  }
+  catch (...) {out_of_memory();}
+
+  int r;
+  uint aPrime, cPrime;
+
+  state[0] = seed % RAND_M;
+  aPrime = RANDMAT_A;
+  cPrime = 1;
+  for (r = 1; r < nr; r++) {
+    state[r] = (RANDMAT_A * state[r - 1] + RANDMAT_C) % RAND_M;
+    cPrime = (cPrime + aPrime) % RAND_M;
+    aPrime = (aPrime * RANDMAT_A) % RAND_M;
+  }
+  cPrime = (cPrime * RANDMAT_C) % RAND_M;
+
+  // fill in random matrix
+  int c;
+  uint v;
+
+#pragma omp parallel for schedule(static) private(c, v)
+  for (r = 0; r < nr; r++) {
+    v = state[r];
+    for (c = 0; c < nc; c++) {
+      MATRIX_RECT(matrix, r, c) = v;
+      v = (aPrime * v + cPrime) % RAND_M;
+    }
+  }
+}
+
