178f768760ea8ca0cd9ac30a4993dc2c1fe90d05
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-06-25 04:53:13 +0000

    hull that exhausts all points

--- cowichan_serial/hull.cpp
+++ cowichan_serial/hull.cpp
@@ -1,34 +1,76 @@
 #include "cowichan_serial.hpp"
 
-void split(PointVector pointsIn, INT64 n, PointVector pointsOut, INT64* hn,
-    Point minPoint, Point maxPoint);
+void quickhull(PointVector pointsIn, INT64 n, PointVector pointsOut,
+    INT64* hn);
 
+void split(PointVector pointsIn, INT64 n, PointVector pointsOut, INT64* hn,
+    Point* p1, Point* p2);
+
+/**
+ * Runs quickhull algorithm until all points have been used up from the
+ * original vector. At each step the hull points are marked as used and a new
+ * convex hull is computed on the rest of points.
+ * The points that have been used up are in the range (n - hn, n), i.e. at
+ * the end of pointsIn vector.
+ * NOTE: pointsIn vector gets modified by the algorithm.
+ */
 void CowichanSerial::hull (PointVector pointsIn, PointVector pointsOut)
 {
-  Point minPoint;
-  Point maxPoint;
+  INT64 hn = 0;
+  INT64 previous_hn = 0;
 
-  minPoint = pointsIn[0];
-  maxPoint = pointsIn[0];
+  // while not all points are used up then run quickhull on the rest of points
+  while (n != hn) {
+    // exclude added points from pointsIn by swapping them with points from the
+    // end of pointsIn vector in range (0, n - nused)
+    INT64 added_i;
+    for (added_i = previous_hn; added_i < hn; added_i++) {
+      // search for the added point
+      for (INT64 i = 0; i < n - previous_hn; i++) {
+        if ((pointsIn[i].x == pointsOut[added_i].x)
+            && (pointsIn[i].y == pointsOut[added_i].y)) {
+          Point tmp = pointsIn[i];
+          pointsIn[i] = pointsIn[n - added_i - 1];
+          pointsIn[n - added_i - 1] = tmp;
+          break;
+        }
+      }
+    }
+    
+    previous_hn = hn;
+    quickhull (pointsIn, n - hn, pointsOut, &hn);
+  }
+}
+
+void quickhull(PointVector pointsIn, INT64 n, PointVector pointsOut,
+    INT64* hn)
+{
+  // base case
+  if (n == 1) {
+    pointsOut[(*hn)++] = pointsIn[0];
+    return;
+  }
+
+  Point* minPoint;
+  Point* maxPoint;
+
+  minPoint = &pointsIn[0];
+  maxPoint = &pointsIn[0];
 
   // figure out the points with minimum and maximum x values
   INT64 i;
-  INT64 hn = 0;
   for (i = 1; i < n; i++) {
-    if (minPoint.x > pointsIn[i].x) {
-      minPoint = pointsIn[i];
+    if (minPoint->x > pointsIn[i].x) {
+      minPoint = &pointsIn[i];
     }
-    if (maxPoint.x < pointsIn[i].x) {
-      maxPoint = pointsIn[i];
+    if (maxPoint->x < pointsIn[i].x) {
+      maxPoint = &pointsIn[i];
     }
   }
 
   // use these as initial pivots
-  split (pointsIn, n, pointsOut, &hn, minPoint, maxPoint);
-  split (pointsIn, n, pointsOut, &hn, maxPoint, minPoint);
-
-  // set the new number of points for chained runs
-  n = hn;
+  split (pointsIn, n, pointsOut, hn, minPoint, maxPoint);
+  split (pointsIn, n, pointsOut, hn, maxPoint, minPoint);
 }
 
 /**
@@ -37,18 +79,18 @@ void CowichanSerial::hull (PointVector pointsIn, PointVector pointsOut)
  * @param n number of input points.
  * @param pointsOut hull output points.
  * @param hn pointer to the number of output points.
- * @param p1 boundary point #1
- * @param p2 boundary point #2
+ * @param p1 boundary point #1.
+ * @param p2 boundary point #2.
  */
 void split (PointVector pointsIn, INT64 n, PointVector pointsOut, INT64* hn,
-    Point p1, Point p2) {
+    Point* p1, Point* p2) {
 
-  Point* maxPoint = NULL;
-  real maxCross = -numeric_limits<real>::infinity ();
+  Point* maxPoint = &pointsIn[0];
+  real maxCross = Point::cross (*p1, *p2, pointsIn[0]);
 
   // compute the signed distances from the line for each point
-  for (INT64 i = 0; i < n; i++) {
-    real currentCross = Point::cross (p1, p2, pointsIn[i]);
+  for (INT64 i = 1; i < n; i++) {
+    real currentCross = Point::cross (*p1, *p2, pointsIn[i]);
     if (currentCross > maxCross) {
       maxPoint = &pointsIn[i];
       maxCross = currentCross;
@@ -59,14 +101,15 @@ void split (PointVector pointsIn, INT64 n, PointVector pointsOut, INT64* hn,
   // if so, it has maximal distance, and we must recurse based on that point.
   if (maxCross > 0.0) {
     // recurse on the new set with the given far point
-    split (pointsIn, n, pointsOut, hn, p1, *maxPoint);
-    split (pointsIn, n, pointsOut, hn, *maxPoint, p2);
+    split (pointsIn, n, pointsOut, hn, p1, maxPoint);
+    split (pointsIn, n, pointsOut, hn, maxPoint, p2);
     return;
   }
 
   // otherwise, it's not on the right side; we don't need to split anymore.
   // this is because all points are inside the hull when we use this half-space.
   // add the first point and return.
-  pointsOut[(*hn)++] = p1;
+  pointsOut[(*hn)++] = *p1;
+
 }
 
