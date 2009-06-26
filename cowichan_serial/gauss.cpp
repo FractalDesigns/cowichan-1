6cedeb0b0595d4c5936799e6573b783a5dc99cde
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-06-26 23:35:41 +0000

    slightly faster serial gauss

--- cowichan_serial/gauss.cpp
+++ cowichan_serial/gauss.cpp
@@ -6,35 +6,46 @@
  */
 void CowichanSerial::gauss (Matrix matrix, Vector target, Vector solution)
 {
-  INT64 r, c, k;
+  INT64 i, j, k;
 
   // forward elimination
-  for (k = 0; k < n; k++) {
-    // calculate pivots in k'th column
-    for (r = k + 1; r < n; r++) {
-      MATRIX_SQUARE(matrix, r, k) = MATRIX_SQUARE(matrix, r, k)
-          / MATRIX_SQUARE(matrix, k, k);
-    }
-    // update elements below k'th row
-    for (r = k + 1; r < n; r++) {
-      for (c = k + 1; c < n; c++) {
-        MATRIX_SQUARE(matrix, r, c) = MATRIX_SQUARE(matrix, r, c)
-            - (MATRIX_SQUARE(matrix, r, k) * MATRIX_SQUARE(matrix, k, c));
+  for (i = 0; i < n; i++) {
+    // get row with maximum column i
+    INT64 max = i;
+    for (j = i + 1; j < n; j++) {
+      if (fabs(MATRIX(matrix, j, i)) > fabs(MATRIX(matrix, max, i))) {
+        max = j;
       }
     }
-    // update element of solution vector
-    for (r = k + 1; r < n; r++) {
-      target[r] = target[r] - (MATRIX_SQUARE(matrix, r, k) * target[k]);
+
+    real tmp;
+    // swap max row with row i
+    for (j = i; j < n; j++) {
+      tmp = MATRIX(matrix, i, j);
+      MATRIX(matrix, i, j) = MATRIX(matrix, max, j);
+      MATRIX(matrix, max, j) = tmp;
+    }
+    tmp = target[i];
+    target[i] = target[max];
+    target[max] = tmp;
+
+    // eliminate i-th column in j-th row
+    real column_i = MATRIX(matrix, i, i);
+    for (j = i + 1; j < n; j++) {
+      real factor = -(MATRIX(matrix, j, i) / column_i);
+      for (k = n - 1; k >= i; k--) {
+        MATRIX(matrix, j, k) += MATRIX(matrix, i, k) * factor;
+      }
+      target[j] += target[i] * factor;
     }
   }
 
   // back substitution
   for (k = (n - 1); k >= 0; k--) {
     solution[k] = target[k] / MATRIX_SQUARE(matrix, k, k);
-    for (r = k - 1; r >= 0; r--) {
-      target[r] = target[r] - (MATRIX_SQUARE(matrix, r, k) * solution[k]);
+    for (i = k - 1; i >= 0; i--) {
+      target[i] = target[i] - (MATRIX_SQUARE(matrix, i, k) * solution[k]);
     }
   }
-
 }
 
