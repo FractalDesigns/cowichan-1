543efc9f4b9f160273f0e981ee4b62eec4007654
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-07-12 17:46:12 +0000

    flipped x/y in serial randmat

--- cowichan_serial/randmat.cpp
+++ cowichan_serial/randmat.cpp
@@ -5,8 +5,8 @@ void CowichanSerial::randmat (IntMatrix matrix)
   index_t r, c;
   INT_TYPE v = seed % RAND_M;
 
-  for (r = 0; r < nr; r++) {
-    for (c = 0; c < nc; c++) {
+  for (c = 0; c < nc; c++) {
+    for (r = 0; r < nr; r++) {
       MATRIX_RECT(matrix, r, c) = v;
       v = (RANDMAT_A * v + RANDMAT_C) % RAND_M;
     }
