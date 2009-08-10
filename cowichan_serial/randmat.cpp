359f817e9511efcfaff7d77e9b649d014d42aafe
c8borzen <c8borzen@766ed74a-8921-11de-91c6-75d7f44f5486>
2009-08-10 22:39:59 +0000

    serial randmat fix

--- cowichan_serial/randmat.cpp
+++ cowichan_serial/randmat.cpp
@@ -11,9 +11,9 @@ void CowichanSerial::randmat (IntMatrix matrix)
   index_t r, c;
   INT_TYPE v = seed % RAND_M;
 
-  for (c = 0; c < nc; c++) {
-    for (r = 0; r < nr; r++) {
-      MATRIX_RECT(matrix, c, r) = v;
+  for (r = 0; r < nr; r++) {
+    for (c = 0; c < nc; c++) {
+      MATRIX_RECT(matrix, r, c) = v;
       v = (RANDMAT_A * v + RANDMAT_C) % RAND_M;
     }
   }
