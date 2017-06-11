662d5d3444122a26d4faa2adb2eb4330b73d7297
Cameron Gorrie <sastraxi@gmail.com>
2017-06-11 15:47:19 -0400

    fix cowichan_tbb/comp64.sh

--- cowichan_tbb/comp64.sh
+++ cowichan_tbb/comp64.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-# not tested
-g++ -Wall -m64 -ltbb -O2 -D LIN64 -c ../cowichan/cowichan.cpp
-g++ -Wall -m64 -ltbb -O2 -D LIN64 -o cowichan_tbb *.cpp cowichan.o
+# Tested on Linux Mint 18.1 (Ubuntu 16.04)
+# Ubuntu has the --as-needed flag enabled, so pass -ltbb as last argument
+# First, sudo apt-get install libtbb-dev
+g++ -Wall -m64 -std=c++11 -O3 -D LIN64 -c ../cowichan/cowichan.cpp -ltbb 
+g++ -Wall -m64 -std=c++11 -O3 -D LIN64 -o cowichan_tbb *.cpp cowichan.o -ltbb 
